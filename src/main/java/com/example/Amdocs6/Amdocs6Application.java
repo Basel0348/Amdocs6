1224ea45fdaa88cb4cc8fb8722dbe6e23b023556
Basel Arkia <basel0348@gmail.com>
2019-01-11 20:24:28 +0200

    First

---
+++ src/main/java/com/example/Amdocs6/Amdocs6Application.java
@@ -0,0 +1,14 @@
+package com.example.Amdocs6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amdocs6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amdocs6Application.class, args);
+	}
+
+}
+
